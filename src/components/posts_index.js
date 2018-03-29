d858134205c2166b2a94637bcacb5853e9497178
Ryan Perlman <ryperl@live.com>
2018-03-28 23:51:18 -0700

    up thru Action Creator Shortcuts 127

---
+++ src/components/posts_index.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+
+
+
+class PostIndex extends Component {
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
+    render() {
+        return (
+            <div>
+                Post Index
+            </div>
+        );
+    }
+}
+
+export default connect(null, { fetchPosts })(PostIndex);
